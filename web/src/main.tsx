8322f1fa276b3fa861c36d5462f234144394de00
Tiago Carioca <tiagoacarioca@gmail.com>
2025-05-30 22:20:25 -0400

    feat: implement react query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,13 +1,21 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+
+import App from '@/App.tsx'
+import { queryClient } from '@/lib/react-query'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</BrowserRouter>
 	</StrictMode>,
 )
