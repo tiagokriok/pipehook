8f1b6a14f14e6722cf34e77f2deeefb97cc959b6
Tiago Carioca <tiagoacarioca@gmail.com>
2025-07-02 20:45:33 -0400

    refactor: add queue type

--- api/internal/core/domain/webhook.go
+++ api/internal/core/domain/webhook.go
@@ -2,14 +2,22 @@ package domain
 
 import "time"
 
+type QueueType string
+
+const (
+	QueueTypeFifo     QueueType = "fifo"
+	QueueTypeStandard QueueType = "standard"
+)
+
 type Webhook struct {
-	ID          string `json:"id"`
-	Label       string `json:"label"`
-	URL         string `json:"url"`
-	Enabled     bool   `json:"enabled"`
-	Slug        string `json:"slug"`
-	Delay       int    `json:"delay"`
-	Concurrency int    `json:"concurrency"`
+	ID          string    `json:"id"`
+	Label       string    `json:"label"`
+	URL         string    `json:"url"`
+	Enabled     bool      `json:"enabled"`
+	Slug        string    `json:"slug"`
+	Delay       int       `json:"delay"`
+	Concurrency int       `json:"concurrency"`
+	Queue       QueueType `json:"queueType"`
 
 	OrganizationID string `json:"organizationId"`
 
